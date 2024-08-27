552704a989f34e9c153e3b636b5e912c2c68851f
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-27 18:51:12 +0300

    исправления в задачу №1 внесены

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
         // Задача №1
         System.out.println("Задача №1");
-        String fistName = "Ivanov";
+        String firstName = "Ivanov";
         String middleName = "Ivan";
         String lastName = "Ivanovich";
-        String fullName = fistName + " " + middleName + " " + lastName;
+        String fullName = firstName + " " + middleName + " " + lastName;
         System.out.println("Ф.И.О. сотрудника - " + fullName);
         // Задача №2
         System.out.println("Задача №2");
-        String fuulName = fistName + " " + middleName + " " + lastName;
+        String fuulName = firstName + " " + middleName + " " + lastName;
         System.out.println(fuulName.toUpperCase());
         // Задача №3
         System.out.println("Задача №3");
