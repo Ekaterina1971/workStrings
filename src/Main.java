3a9f0fcf68a47545ef85d8fc9875bcedee75466f
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-30 17:06:18 +0300

    исправления внесены

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void printLeapYearStatus(int year) {
         if (year > 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
@@ -7,13 +9,14 @@ public class Main {
         }
     }
     public static void applicationSelection(int clientOs, int clientDeviceYear){
-        if (clientOs == 0 && clientDeviceYear == 2024) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientOs == 0 && clientDeviceYear == currentYear) {
             System.out.println("Установите версию приложения iOS по ссылке.");
-        } else if (clientOs == 0 && clientDeviceYear < 2024) {
+        } else if (clientOs == 0 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения iOS по ссылке.");
-        } else if (clientOs == 1 && clientDeviceYear == 2024) {
+        } else if (clientOs == 1 && clientDeviceYear == currentYear) {
           System.out.println("Установите версию приложения для Android по ссылке.");
-        } else if (clientOs == 1 && clientDeviceYear < 2024) {
+        } else if (clientOs == 1 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке.");
         }
     }
@@ -31,12 +34,12 @@ public class Main {
     public static void main(String[] args) {
        // Задача №1
         System.out.println("задача №1");
-        int year = 2020;
+        int year = 2021;
        printLeapYearStatus(year);
        // Задача №2
         System.out.println("Задача №2");
         int clientOs = 0;
-        int clientDeviceYear = 2020;
+        int clientDeviceYear = 2023;
         applicationSelection(clientOs, clientDeviceYear);
         // Задача №3
         System.out.println("Задача №3");
