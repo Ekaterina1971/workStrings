43fe68e57b6f30b35472aea175365a1f2a9a0dd4
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-30 11:25:09 +0300

    задача №2

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,26 @@ public class Main {
             System.out.println(year + " год не является високосным");
         }
     }
+    public static void applicationSelection(int clientOs, int clientDeviceYear){
+        if (clientOs == 0 && clientDeviceYear == 2024) {
+            System.out.println("Установите версию приложения iOS по ссылке.");
+        } else if (clientOs == 0 && clientDeviceYear < 2024) {
+            System.out.println("Установите облегченную версию приложения iOS по ссылке.");
+        } else if (clientOs == 1 && clientDeviceYear == 2024) {
+          System.out.println("Установите версию приложения для Android по ссылке.");
+        } else if (clientOs == 1 && clientDeviceYear < 2024) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        }
+    }
     public static void main(String[] args) {
-       int year = 2020;
+       // Задача №1
+        System.out.println("задача №1");
+        int year = 2020;
        printLeapYearStatus(year);
+       // Задача №2
+        System.out.println("Задача №2");
+        int clientOs = 0;
+        int clientDeviceYear = 2020;
+        applicationSelection(clientOs, clientDeviceYear);
     }
 }
