4bcf74b3a23cebf4bb14ea9daf4cc2ff1f5eb2ea
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-30 10:39:36 +0300

    задача №1

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
 public class Main {
+    public static void printLeapYearStatus(int year) {
+        if (year > 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
     public static void main(String[] args) {
-        // Задача №1
-        System.out.println("Задача №1");
-        String firstName = "Ivanov";
-        String middleName = "Ivan";
-        String lastName = "Ivanovich";
-        String fullName = firstName + " " + middleName + " " + lastName;
-        System.out.println("Ф.И.О. сотрудника - " + fullName);
-        // Задача №2
-        System.out.println("Задача №2");
-        String fuulName = firstName + " " + middleName + " " + lastName;
-        System.out.println(fuulName.toUpperCase());
-        // Задача №3
-        System.out.println("Задача №3");
-        String fullName1 = "Иванов Семён Семёнович";
-        String fullNameTrue = fullName1.replace("ё", "е");
-        System.out.println("Данные Ф.И.О. сотрудника - " + fullNameTrue);
+       int year = 2020;
+       printLeapYearStatus(year);
     }
 }
